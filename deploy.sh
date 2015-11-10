d3843ec299b59a47d96aeb2d0b7ca77a3cc14cc3
Andrew Cleasby <acleasby@infinio.com>
2015-11-10 12:46:26 -0500

    Bump version for release

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-VERSION=1.0.2-SNAPSHOT
-REPOSITORY=snapshots
+VERSION=1.0.3
+REPOSITORY=releases
 
 mvn deploy:deploy-file -Durl="https://admin:n1c3NC0mpl3x!@nexus.infinio.com/content/repositories/$REPOSITORY" -Dfile=dist/wbemservices/bin/mofcomp.jar -DgroupId=org.sourceforge.wbemservices -DartifactId=mofcomp -Dversion=$VERSION -Dpackaging=jar
 
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=1.0.2-SNAPSHOT
+VERSION=1.0.3
 REPOSITORY=snapshots
 
 mvn install:install-file -Durl="https://admin:n1c3NC0mpl3x!@nexus.infinio.com/content/repositories/$REPOSITORY" -Dfile=dist/wbemservices/bin/mofcomp.jar -DgroupId=org.sourceforge.wbemservices -DartifactId=mofcomp -Dversion=$VERSION -Dpackaging=jar
